a0b63d95c712ee05df61e8f72cafdba86772e0e4
rics3n <rico@snib.it>
2016-03-04 12:47:13 +0100

    fix original_filename

--- lib/carrierwave-meta/meta.rb
+++ lib/carrierwave-meta/meta.rb
@@ -9,7 +9,7 @@ module CarrierWave
       include CarrierWave::ModelDelegateAttribute
       include CarrierWave::MimeTypes
 
-      set_content_type(true)
+      #set_content_type(true)
 
       #after :retrieve_from_cache, :set_content_type
       after :retrieve_from_cache, :call_store_meta
